f8a6877faa6f21b4fae515bfe29caf0720ae394e
Anisa <anisa.ali7@gmail.com>
2017-04-12 11:20:26 -0500

    variables

--- client/scripts/factories/PizzaService.js
+++ client/scripts/factories/PizzaService.js
@@ -1,6 +1,15 @@
 pizzaApp.factory('PizzaService', function() {
   var test = "testing testing 123";
 
+  var pizzaObj = {
+    sm: 6,
+    md: 7,
+    lg: 8
+  };
+  var toppings = [];
+  //toppings: salmon, mushroom, pinneapple, macNcheese, shrimp
+  var pizzaOrder = {};
+  var orderedPizza = [];
 
   return {
     test: test
